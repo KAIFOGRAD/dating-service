06e74b0b91116c99a88e17aabc4d6792caf7a42b
unknown <ostanin8900@yandex.ru>
2025-02-11 22:44:48 +0500

    eureka init

---
+++ eureka/src/main/java/com/dating/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.dating.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RefrshScope
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
