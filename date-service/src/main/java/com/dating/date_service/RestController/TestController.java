bc6b83cffe8dc6f6f8f76079b1708d837e407e1c
unknown <ostanin8900@yandex.ru>
2025-02-12 23:27:14 +0500

    add testCOntroller

--- date-service/src/main/java/com/dating/date_service/RestController/TestController.java
+++ date-service/src/main/java/com/dating/date_service/RestController/TestController.java
@@ -1,5 +1,16 @@
 package com.dating.date_service.RestController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/v1/test")
 public class TestController {
+
+    @GetMapping("hello")
+    public String test() {
+        return "hello";
+    }
 
 }
